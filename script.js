0b770b6282eba000fbda36c0c12cb5b3dfe20453
Nick M <mully7773@gmail.com>
2022-05-17 23:03:13 -0500

    Reset the game to starting position

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-'use strict';
+"use strict";
+
+// Selecting elements
+const scoreZeroEl = document.querySelector("#score--0");
+const scoreOneEl = document.getElementById("score--1");
+const diceEl = document.querySelector(".dice");
+
+scoreZeroEl.textContent = 0;
+scoreOneEl.textContent = 0;
+diceEl.classList.add("hidden");
